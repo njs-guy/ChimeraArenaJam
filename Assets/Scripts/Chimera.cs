19370a1cbbfd4fa96c05ed71c2d78ff24f28ab27
Nick Shockey <shockeynj@gmail.com>
2021-06-12 11:11:21 -0400

    Added code to change body sprites

--- Assets/Scripts/Chimera.cs
+++ Assets/Scripts/Chimera.cs
@@ -25,21 +25,115 @@ public class Chimera : MonoBehaviour
     [SerializeField] public Animal headPart;
     [SerializeField] public Animal bodyPart;
     [SerializeField] public Animal legPart;
-    [SerializeField] public Sprite headSprite;
-    [SerializeField] public Sprite bodySprite;
-    [SerializeField] public Sprite legsSprite;
+    [SerializeField] public Sprite[] headSprites;
+    [SerializeField] public Sprite[] bodySprites;
+    [SerializeField] public Sprite[] legSprites;
 
-    void updatePart()
+    //Assigns each part sprite
+    void updateParts()
     {
-        head.sprite = headSprite;
-        torso.sprite = bodySprite;
-        legs.sprite = legsSprite;
+        //checks what animal each part should be, and changes the sprite
+        Sprite currentHead;
+        Sprite currentBody;
+        Sprite currentLegs;
+
+
+        switch(headPart)
+        {
+            case Animal.bird:
+                currentHead = headSprites[0];
+                Debug.Log("bird head");
+                break;
+            case Animal.bunny:
+                currentHead = headSprites[1];
+                Debug.Log("bunny head");
+                break;
+            case Animal.monkey:
+                currentHead = headSprites[2];
+                Debug.Log("monkey head");
+                break;
+            case Animal.octopus:
+                currentHead = headSprites[3];
+                Debug.Log("octopus head");
+                break;
+            case Animal.turtle:
+                currentHead = headSprites[4];
+                Debug.Log("turtle head");
+                break;
+            default:
+                currentHead = headSprites[2];
+                Debug.Log("else, return to monke");
+                break;
+            
+        }
+
+        switch(bodyPart)
+        {
+            case Animal.bird:
+                currentBody = bodySprites[0];
+                Debug.Log("bird body");
+                break;
+            case Animal.bunny:
+                currentBody = bodySprites[1];
+                Debug.Log("bunny body");
+                break;
+            case Animal.monkey:
+                currentBody = bodySprites[2];
+                Debug.Log("monkey body");
+                break;
+            case Animal.octopus:
+                currentBody = bodySprites[3];
+                Debug.Log("octopus body");
+                break;
+            case Animal.turtle:
+                currentBody = bodySprites[4];
+                Debug.Log("turtle body");
+                break;
+            default:
+                currentBody = bodySprites[2];
+                Debug.Log("else, return to monke");
+                break;
+
+        }
+
+        switch(legPart)
+        {
+            case Animal.bird:
+                currentLegs = legSprites[0];
+                Debug.Log("bird legs");
+                break;
+            case Animal.bunny:
+                currentLegs = legSprites[1];
+                Debug.Log("bunny legs");
+                break;
+            case Animal.monkey:
+                currentLegs = legSprites[2];
+                Debug.Log("monkey legs");
+                break;
+            case Animal.octopus:
+                currentLegs = legSprites[3];
+                Debug.Log("octopus legs");
+                break;
+            case Animal.turtle:
+                currentLegs = legSprites[4];
+                Debug.Log("turtle legs");
+                break;
+            default:
+                currentLegs = legSprites[2];
+                Debug.Log("else, return to monke");
+                break;
+
+        }
+
+        head.sprite = currentHead;
+        torso.sprite = currentBody;
+        legs.sprite = currentLegs;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        updatePart();
+        updateParts();
     }
 
     // Update is called once per frame
